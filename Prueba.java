efba552e02723ea4a2818c031b9e901a11db99dd
Samuel <samualonso4@gmail.com>
2020-10-06 09:04:03 +0200

    Línea 5

--- Prueba.java
+++ Prueba.java
@@ -1,10 +1,11 @@
-public class Prueba.java {
+public class Prueba {
 
-	public static void main.(String args[]) {
+	public static void main(String args[]) {
 
 		System.out.println("Samuel Alonso Gonzalez");
 		System.out.println("Leon, 2001");
 		System.out.println("Leon, Windows 10 Home");
+		System.out.println("Samuel Alonso Gonzalez");
 
 	}
 
